28c278079407ea1e2963bda9f85ce1025d9d3d4f
RennanD <rennan.douglas@objetivopi.com.br>
2021-08-09 17:55:23 -0300

    chore: init product details screen

--- src/screens/Details/styles.ts
+++ src/screens/Details/styles.ts
@@ -4,34 +4,34 @@ export const Container = styled.View``;
 
 export const Header = styled.View``;
 
-export const BackButton = styled.View``;
+export const BackButton = styled.TouchableOpacity``;
 
 export const CouldBadge = styled.View``;
 
 export const ImageContainer = styled.View``;
 
-export const ProductImage = styled.View``;
+export const ProductImage = styled.Image``;
 
 export const ProductDetails = styled.View``;
 
-export const ProductDescription = styled.View``;
+export const ProductDescription = styled.Text``;
 
-export const ProductPrice = styled.View``;
+export const ProductPrice = styled.Text``;
 
 export const CheckoutContainer = styled.View``;
 
 export const ProductQuantityInput = styled.View``;
 
-export const RemoveAmountProductButton = styled.View``;
+export const RemoveAmountProductButton = styled.TouchableOpacity``;
 
-export const AddAmountProductButton = styled.View``;
+export const AddAmountProductButton = styled.TouchableOpacity``;
 
 export const AddManyProductsContainer = styled.View``;
 
-export const AddManyProductsButton = styled.View``;
+export const AddManyProductsButton = styled.TouchableOpacity``;
 
-export const AddManyProductsText = styled.View``;
+export const AddManyProductsText = styled.Text``;
 
-export const AddToCartButton = styled.View``;
+export const AddToCartButton = styled.TouchableOpacity``;
 
-export const AddToCartButtonText = styled.View``;
+export const AddToCartButtonText = styled.Text``;
